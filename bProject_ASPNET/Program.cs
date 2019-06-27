858bf1a150b4494be0833d1f6b78d1b3a16f6962
falcon78_infodex <falconhawk@protonmail.com>
2019-06-27 17:58:38 +0900

    Added config files

--- bProject_ASPNET/Program.cs
+++ bProject_ASPNET/Program.cs
@@ -17,8 +17,8 @@ namespace bProject_ASPNET
             CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>  
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-    }
+    } 
 }
--- bProject_ASPNET/Startup.cs
+++ bProject_ASPNET/Startup.cs
@@ -5,14 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace bProject_ASPNET
 {
     public class Startup
     {
+        private IConfiguration _config;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
@@ -27,7 +38,8 @@ namespace bProject_ASPNET
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "what"); 
+                await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName + "what");
+                await context.Response.WriteAsync(_config["MyKey"]);
             });
         }
     }
