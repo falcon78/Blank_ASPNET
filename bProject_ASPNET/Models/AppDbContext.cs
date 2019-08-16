21f670263458e24f29710c845f5aa436bdc632a5
falcon78_infodex <falconhawk@protonmail.com>
2019-08-16 19:26:33 +0900

    added new context

--- bProject_ASPNET/Models/AppDbContext.cs
+++ bProject_ASPNET/Models/AppDbContext.cs
@@ -9,14 +9,6 @@ public class AppDbContext : DbContext
     public DbSet<User> Users { get; set; }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>().HasData(
-            new User
-            {
-                ID = 3,
-                Name = "First User",
-                UserHobby = Hobby.Basketball,
-                Email = "firstuser@gmail.com"
-            }
-        );
+        modelBuilder.Seed();
     }
 }
--- bProject_ASPNET/Models/ModelBuilderExtensions.cs
+++ bProject_ASPNET/Models/ModelBuilderExtensions.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace bProject_ASPNET.Models
 {
-    public class ModelBuilderExtensions
+    public static class ModelBuilderExtensions
     {
+        public static void Seed( this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(
+                       new User
+                       {
+                           ID = 3,
+                           Name = "First User",
+                           UserHobby = Hobby.Basketball,
+                           Email = "firstuser@gmail.com"
+                       }
+            );
+        }
     }
 }
